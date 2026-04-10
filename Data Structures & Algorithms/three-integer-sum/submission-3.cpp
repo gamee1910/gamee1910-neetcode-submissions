6198c544d6e1f2108eeccab5659967220facd348
Hoang Hai Hau <168702939+gamee1910@users.noreply.github.com>
2026-04-10 10:54:32 +0700

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.cpp
@@ -0,0 +1,61 @@
+// class Solution {
+// public:
+//     vector<vector<int>> threeSum(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         vector<vector<int>> res;
+
+//         for(int i = 0; i < nums.size(); i++){
+//             if(nums[i] > 0) break;
+//             if(i > 0 && nums[i] == nums[i - 1]) continue;
+
+//             int left = i + 1, right = nums.size() - 1;
+//             while(left < right){
+//                 int sum = nums[i] + nums[left] + nums[right];
+//                 if(sum < 0){
+//                     right--;
+//                 } else if(sum > 0){
+//                     left++;
+//                 } else {
+//                     res.push_back({nums[i], nums[left], nums[right]});
+//                     left++;
+//                     right--;
+//                     while(left < right && nums[left] == nums[left - 1]){
+//                         left++;
+//                     }
+//                 }
+//             }
+//         }   
+//         return res;
+//     }
+// };
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int l = i + 1, r = nums.size() - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum > 0) {
+                    r--;
+                } else if (sum < 0) {
+                    l++;
+                } else {
+                    res.push_back({nums[i], nums[l], nums[r]});
+                    l++;
+                    r--;
+                    while (l < r && nums[l] == nums[l - 1]) {
+                        l++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
