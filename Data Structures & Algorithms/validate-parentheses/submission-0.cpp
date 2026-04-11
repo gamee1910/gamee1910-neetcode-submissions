2e8ba269d598a916e0c06c8f441662051001e19a
Hoang Hai Hau <168702939+gamee1910@users.noreply.github.com>
2026-04-11 15:40:45 +0700

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+        unordered_map<char, char> map = {
+            {']', '['},
+            {']', '['},
+
+        }
+    }
+};  
